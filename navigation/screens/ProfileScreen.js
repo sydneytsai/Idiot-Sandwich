68cc06f4d39fcf66ce9ccf3acf6ab8ac10ae0bfc
Sydney Tsai <sydneyctsai@gmail.com>
2022-11-25 14:14:11 -0800

    profile

--- navigation/screens/ProfileScreen.js
+++ navigation/screens/ProfileScreen.js
@@ -1,15 +1,39 @@
 import * as React from 'react';
-import { View, Text, Button} from 'react-native';
+import { View, Text, Button, SafeAreaView, StyleSheet } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 function ProfileScreen({navigation}) {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', }}>
-            <Text
-                onPress={() => navigation.navigate('Home')}
-                style={{ fontSize: 26, fontWeight: 'bold' }}>Profile Screen
+        <SafeAreaView style={styles.profileContainer}>
+            <Ionicons name="settings-outline" style={styles.settingsIcon}>
+            </Ionicons>
+            <Text style={styles.headers}>
+                Jane Doe
             </Text>
-        </View>
+            <Text style={styles.headers}>
+                Collection
+            </Text>
+            <Text style={styles.headers}>
+                Tags
+            </Text>
+        </SafeAreaView>
     );
 };
+
+const styles = StyleSheet.create({
+    profileContainer: {
+        flex: 1, 
+        flexDirection: "column",
+        alignItems: 'center', 
+    },
+    headers: {
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    settingsIcon: {
+        size: 40,
+        color: "lightgrey",
+    }
+});
 
 export default ProfileScreen
