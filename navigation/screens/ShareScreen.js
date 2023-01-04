010a9ce91318a061f926d2260f3e099a00c7dc3f
Sydney Tsai <sydneyctsai@gmail.com>
2023-01-05 00:34:40 +0800

    floating button

--- navigation/screens/ShareScreen.js
+++ navigation/screens/ShareScreen.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { SafeAreaView, Text, StyleSheet} from 'react-native';
+import { SafeAreaView, StyleSheet, TouchableOpacity, Text} from 'react-native';
 import { ScrollView } from 'react-native-gesture-handler';
 
 function ShareScreen({navigation}) {
@@ -8,6 +8,24 @@ function ShareScreen({navigation}) {
             <ScrollView style={styles.shareScrollView} contentContainerStyle={styles.contentContainer}>
 
             </ScrollView>
+            <TouchableOpacity
+                style={{
+                    borderWidth: 1,
+                    borderColor: '#75975e',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    width: 70,
+                    position: 'absolute',
+                    top: '85%',
+                    right: 20,
+                    height: 70,
+                    backgroundColor: '#75975e',
+                    borderRadius: 100,
+                }}
+                onPress={() => { alert('Button is pressed') }}
+            >
+                <Text style={{flex:.8, color: 'white', fontSize: 40}}>+</Text>
+            </TouchableOpacity>
         </SafeAreaView>
     );
 };
